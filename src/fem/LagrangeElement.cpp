ec1253f01aece36cddd998b636e0396c57cf0673
Max Winkler <max.winkler@mathematik.tu-chemnitz.de>
2018-06-14 23:51:00 +0200

    Implemented P3 Lagrange elements

--- src/fem/LagrangeElement.cpp
+++ src/fem/LagrangeElement.cpp
@@ -1,5 +1,9 @@
 #include "fem/LagrangeElement.h"
 
+#include "linalg/DenseMatrix.h"
+
+using chemfem::linalg::DenseMatrix;
+
 namespace chemfem{
   namespace fem{
     
@@ -37,7 +41,8 @@ namespace chemfem{
 	    {
 	      int edge_ind = (i-3) / 2;
 	      int vert_ind = (i-3) % 2;  
-	      return 13.5 * lambda[i]*lambda[(i+1)%3]*(lambda[(edge_ind+vert_ind)%3]-1./3);
+	      return 13.5 * lambda[edge_ind]*lambda[(edge_ind+1)%3]
+		*(lambda[(edge_ind+vert_ind)%3]-1./3);
 	    }
 	  else if(i==9)
 	    return 27*lambda[0]*lambda[1]*lambda[2];
@@ -53,32 +58,51 @@ namespace chemfem{
     Vector LagrangeElement::Gradient(int i, double x, double y) const
     {           
       double lambda[3] = {1.-x-y, x, y};
-      Vector gradLambda[3] = {Vector(2), Vector(2), Vector(2)};
 
-      Vector grad(2);
-      
-      gradLambda[0][0] = -1.;
-      gradLambda[0][1] = -1.;
+      DenseMatrix dLdX(2,3);
+      dLdX[0][0] = -1.; dLdX[0][1] = 1.; dLdX[0][2] = 0.;
+      dLdX[1][0] = -1.; dLdX[1][1] = 0.; dLdX[1][2] = 1.;
 
-      gradLambda[1][0] = 1.;
-      gradLambda[1][1] = 0.;
-
-      gradLambda[2][0] = 0.;
-      gradLambda[2][1] = 1.;
-      
+      Vector grad_L(3);
+            
       switch(degree)
 	{
 	case 1:
-	  grad = gradLambda[i];
-	  return grad;
+	  if(i<3)
+	    grad_L[i] = 1.;
+	  break;
 	case 2:
 	  if(i<3)
-	    return (2*lambda[i]-1.)*gradLambda[i] + 2*lambda[i]*gradLambda[i];
-	  else
-	    return 4*(lambda[i-3]*gradLambda[(i-2)%3] + lambda[(i-2)%3]*gradLambda[i-3]);
+	    grad_L[i] = 4.*lambda[i] -1.;
+	  else if(i<6)
+	    {
+	      grad_L[i] = 4*lambda[(i+1)%3];
+	      grad_L[(i+1)%3] = 4*lambda[i];
+	    }
+	  break;
+	case 3:
+	  if(i<3)
+	    grad_L[i] = 4.5*(lambda[i]*(lambda[i]-1./3) + (2*lambda[i]-1./3)*(lambda[i]-2./3));
+	  else if(i<9)
+	    {
+	      int edge_ind = (i-3) / 2;
+	      int vert_ind = (i-3) % 2;
+
+	      grad_L[edge_ind] = 4.5*lambda[(edge_ind+1)%3]
+		*(lambda[edge_ind + vert_ind]-1./3 + (vert_ind == 0 ? lambda[edge_ind] : 0.));
+	      grad_L[(edge_ind+1)%3] = 4.5*lambda[edge_ind]
+		*(lambda[edge_ind + vert_ind]-1./3 + (vert_ind == 1 ? lambda[(edge_ind+1)%3] : 0.));
+	    }
+	  else if(i==10)
+	    {
+	      grad_L[0] = 27.*lambda[1]*lambda[2];
+	      grad_L[1] = 27.*lambda[0]*lambda[2];
+	      grad_L[2] = 27.*lambda[0]*lambda[1];
+	    }
+	    break;
 	}
 
-      return Vector();
+      return dLdX*grad_L;
     }
     
   }
